1394db31164a8bd0a55a10ac9e2877d6bbb0b53c
Ian rizki <ian.rizki2@gmail.com>
2018-12-24 16:42:14 +0700

    add alert box

--- resources/views/include/info.blade.php
+++ resources/views/include/info.blade.php
@@ -1,22 +1,6 @@
 @if (session('message'))
-<div id="myModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header</h4>
-      </div>
-      <div class="modal-body">
-        <p>{{session('message')}}</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-
-  </div>
+<div class="alert alert-success">
+  <strong>Success!</strong> {{session('message')}}.
 </div>
 @endif
 
